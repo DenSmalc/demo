7335867a2259736a51fa8eabc353e5d2a84541b7
DenSmalc <95161399+DenSmalc@users.noreply.github.com>
2026-02-13 09:36:44 +0300

    Delete demo/isp directory

--- demo/isp/route.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-hostnamectl set-hostname isp; exec bash
-echo ‘172.16.1.14/28’ > /etc/net/ifaces/ens20/ipv4address
-echo ‘172.16.2.14/28’ > /etc/net/ifaces/ens21/ipv4address
-systemctl restart network
-apt-get update
-apt-get install nano nftables -y
-nano /etc/nftables/nftables.nf
