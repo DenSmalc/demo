1d65a5464f499ec657f99686ccc033509dd90654
DenSmalc <95161399+DenSmalc@users.noreply.github.com>
2026-02-13 09:42:25 +0300

    Add files via upload

---
+++ isp/nft.sh
@@ -0,0 +1,7 @@
+hostnamectl set-hostname isp; exec bash
+echo ‘172.16.1.14/28’ > /etc/net/ifaces/ens20/ipv4address
+echo ‘172.16.2.14/28’ > /etc/net/ifaces/ens21/ipv4address
+systemctl restart network
+apt-get update
+apt-get install nano nftables -y
+echo "Okey"
